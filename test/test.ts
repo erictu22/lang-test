54e59a5bb1e2758a3ca6df5db5bf0b98ae0ca7cc
erictu22 <erictu222@gmail.com>
2023-06-10 18:13:12 -0400

    adds openai + splits unit tests and api into two separate files

--- test/test.ts
+++ test/test.ts
@@ -1,19 +1,5 @@
 import request from 'supertest';
-import express, { Request, Response } from 'express';
-
-const app = express();
-
-app.use(express.json());
-
-app.post('/', (req: Request, res: Response) => {
-  const { name, age } = req.body;
-
-  if (typeof age !== 'number' || age < 0) {
-    return res.status(400).json({ error: 'Invalid age. Age must be a positive number.' });
-  }
-
-  res.send(`Hello ${name}! Your age is ${age}.`);
-});
+import app from '../src/index';
 
 describe('POST /', () => {
   it('should return a greeting with name and age', async () => {
